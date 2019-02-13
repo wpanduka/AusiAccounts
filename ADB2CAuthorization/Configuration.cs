5ed30d778a84707e27d6f0fe6fab1c2690e79060
Panduka weerasekara <wpanduka314@gmail.com>
2019-02-13 21:35:22 +0530

    Update Configuration.cs

--- ADB2CAuthorization/Configuration.cs
+++ ADB2CAuthorization/Configuration.cs
@@ -7,6 +7,6 @@ namespace ADB2CAuthorization
     public static class Configuration
     {
 
-        public const string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=wpanduka;AccountKey=a7yreWgAA3kUiG4u2igZ1KaJJD1Ss2P97B9o39GKTb9vmIRL0SsVVEiTmHxuPzPE8CLyK/h+UFc5OITP8f+UiQ==;EndpointSuffix=core.windows.net";
+        public const string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=wpanduka;AccountKey=YOUR KEY GOES HEAR;EndpointSuffix=core.windows.net";
     }
 }
